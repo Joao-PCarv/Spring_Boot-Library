ecd902e0076fa3799e62d6bd783eec49f537cdeb
joaopc <1232324@isep.ipp.pt>
2026-02-27 12:08:51 +0000

    add LivroRepository interface for CRUD operations on Livro entity

---
+++ src/main/java/github/jpc/libraryapi/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package github.jpc.libraryapi.repository;
+
+import github.jpc.libraryapi.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LivroRepository extends JpaRepository<Livro, UUID> {
+}
