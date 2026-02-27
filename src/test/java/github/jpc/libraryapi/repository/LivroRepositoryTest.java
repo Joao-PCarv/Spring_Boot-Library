a05ae052a4d3a7d1a57ca0edf6d9b8af84495a55
joaopc <1232324@isep.ipp.pt>
2026-02-27 16:33:25 +0000

    add tests for updating and deleting Livro entities in LivroRepositoryTest

--- src/test/java/github/jpc/libraryapi/repository/LivroRepositoryTest.java
+++ src/test/java/github/jpc/libraryapi/repository/LivroRepositoryTest.java
@@ -6,6 +6,7 @@ import github.jpc.libraryapi.model.Livro;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -27,7 +28,7 @@ class LivroRepositoryTest {
         livro.setIsbn("978-85-508-0439-8");
         livro.setPreco(BigDecimal.valueOf(100));
         livro.setGenero(GeneroLivro.FICCAO);
-        livro.setTitulo("Outro livro");
+        livro.setTitulo("Outro livro 2");
         livro.setDataPublicacao(LocalDate.of(2020, 10, 10));
 
         // Recupera o autor existente no banco de dados usando o ID e associa o autor ao livro antes de salvar
@@ -61,4 +62,37 @@ class LivroRepositoryTest {
 
         livroRepository.save(livro);
     }
+
+    @Test
+    void atualizarAutorDoLivro(){
+        UUID id = UUID.fromString("243ea91e-ef1b-4e46-9677-a85c53c6760f");
+        var livroParaAtualizar = livroRepository.findById(id).orElse(null);
+
+        UUID id1 = UUID.fromString("2018f827-355b-4c44-9bfc-b3056184374d");
+        Autor maria = autorRepository.findById(id1).orElse(null);
+
+        livroParaAtualizar.setAutor(maria);
+
+        livroRepository.save(livroParaAtualizar);
+    }
+
+    @Test
+    void delete(){
+        UUID id = UUID.fromString("243ea91e-ef1b-4e46-9677-a85c53c6760f");
+        livroRepository.deleteById(id);
+    }
+
+    @Test
+    @Transactional
+    void buscarLivroTeste(){
+        UUID id  = UUID.fromString("3a39abfc-10b9-4c5e-9705-6c042721d75a");
+        Livro livro = livroRepository.findById(id).orElse(null);
+
+        System.out.println("Livro encontrado: ");
+        System.out.println(livro.getTitulo());
+
+        System.out.println("Autor do livro: ");
+        System.out.println(livro.getAutor().getNome());
+    }
+
 }
