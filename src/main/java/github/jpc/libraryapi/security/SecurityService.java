ec7cc718efcc3b4571e9e08a51a94c3641316678
joaopc <1232324@isep.ipp.pt>
2026-03-28 15:17:43 +0000

    add SecurityService class for managing user authentication and retrieval

---
+++ src/main/java/github/jpc/libraryapi/security/SecurityService.java
@@ -0,0 +1,29 @@
+package github.jpc.libraryapi.security;
+
+import github.jpc.libraryapi.model.Usuario;
+import github.jpc.libraryapi.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityService {
+
+    private final UsuarioService usuarioService;
+
+    public Usuario obterUsuarioLogado(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+//        String login = userDetails.getUsername();
+//        return usuarioService.obterPorLogin(login);
+
+        if(authentication instanceof CustomAuthentication customAuthentication){
+            return customAuthentication.getUsuario();
+        }
+        return null;
+    }
+
+}
