3dc7f04b6c2c7251dedf8f6d6fd2e0503ff51d8d
joaopc <1232324@isep.ipp.pt>
2026-03-28 15:18:52 +0000

    add GlobalExceptionHandler for centralized exception handling

---
+++ src/main/java/github/jpc/libraryapi/controller/common/GlobalExceptionHandler.java
@@ -0,0 +1,76 @@
+package github.jpc.libraryapi.controller.common;
+
+import github.jpc.libraryapi.controller.dto.ErroCampo;
+import github.jpc.libraryapi.controller.dto.ErroResposta;
+import github.jpc.libraryapi.exceptions.CampoInvalidoException;
+import github.jpc.libraryapi.exceptions.OperacaoNaoPermitidaException;
+import github.jpc.libraryapi.exceptions.RegistoDuplicadoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErroResposta handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        List<FieldError> fieldErrors = e.getFieldErrors();
+        List<ErroCampo> listaErros = fieldErrors
+                .stream()
+                .map(fe -> new ErroCampo(fe.getField(), fe.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return new ErroResposta(
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Erro de validação.",
+                listaErros);
+    }
+
+    @ExceptionHandler(RegistoDuplicadoException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErroResposta handleRegistroDuplicadoException(RegistoDuplicadoException e){
+        return ErroResposta.conflito(e.getMessage());
+    }
+
+    @ExceptionHandler(OperacaoNaoPermitidaException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErroResposta handleOperacaoNaoPermitidaException(
+            OperacaoNaoPermitidaException e){
+        return ErroResposta.respostaPadrao(e.getMessage());
+    }
+
+    @ExceptionHandler(CampoInvalidoException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErroResposta handleCampoInvalidoException(CampoInvalidoException e){
+        return new ErroResposta(
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Erro de validação.",
+                List.of(new ErroCampo(e.getCampo(), e.getMessage())));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErroResposta handleErrosNaoTratados(RuntimeException e){
+        System.out.println(e.getMessage());
+        return new ErroResposta(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Ocorreu um erro inesperado. Entre em contato com a administração."
+                , List.of());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErroResposta handleAcessDeniedException(AccessDeniedException e){
+        return new ErroResposta(
+                HttpStatus.FORBIDDEN.value(),
+                "Acesso negado. Você não tem permissão para acessar este recurso."
+                , List.of());
+    }
+}
