6391a50adb2243e0a0c9281388f6aefb81d3bd53
joaopc <1232324@isep.ipp.pt>
2026-03-28 15:19:39 +0000

    add query methods to AutorRepository for enhanced author search capabilities

--- src/main/java/github/jpc/libraryapi/repository/AutorRepository.java
+++ src/main/java/github/jpc/libraryapi/repository/AutorRepository.java
@@ -3,8 +3,18 @@ package github.jpc.libraryapi.repository;
 
 import github.jpc.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 // Não é necessário colocar annotation @Repository, pois a interface JpaRepository já é reconhecida como um componente de repositório pelo Spring Data JPA
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
+
+    List<Autor> findByNome(String nome);
+    List<Autor> findByNacionalidade(String nacionalidade);
+    List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
+
+    Optional<Autor> findByNomeAndDataNascimentoAndNacionalidade(String nome, LocalDate dataNascimento, String nacionalidade);
 }
