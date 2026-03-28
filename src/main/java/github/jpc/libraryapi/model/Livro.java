c32224b3aabbf6f795f9427c5b46b07b46819683
joaopc <1232324@isep.ipp.pt>
2026-03-28 15:18:49 +0000

    add auditing fields and relationships to Livro entity

--- src/main/java/github/jpc/libraryapi/model/Livro.java
+++ src/main/java/github/jpc/libraryapi/model/Livro.java
@@ -2,14 +2,21 @@ package github.jpc.libraryapi.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name="livro")
+@Table(name = "livro")
 @Data
+@ToString(exclude = "autor")
+@EntityListeners(AuditingEntityListener.class)
 public class Livro {
 
     @Id
@@ -37,7 +44,19 @@ public class Livro {
 //            cascade = CascadeType.ALL
         fetch = FetchType.LAZY
     )
-    @JoinColumn(name="id_autor")
+    @JoinColumn(name = "id_autor")
     private Autor autor;
+
+    @CreatedDate
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
 
 }
