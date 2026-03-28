5ab69953cbba517e9e6a014e44e1695e2d7b038b
joaopc <1232324@isep.ipp.pt>
2026-03-28 15:19:51 +0000

    add AutorController for managing author-related operations

---
+++ src/main/java/github/jpc/libraryapi/controller/AutorController.java
@@ -0,0 +1,104 @@
+package github.jpc.libraryapi.controller;
+
+import github.jpc.libraryapi.controller.dto.AutorDTO;
+import github.jpc.libraryapi.controller.mappers.AutorMapper;
+import github.jpc.libraryapi.model.Autor;
+import github.jpc.libraryapi.model.Usuario;
+import github.jpc.libraryapi.security.SecurityService;
+import github.jpc.libraryapi.service.AutorService;
+import github.jpc.libraryapi.service.UsuarioService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("autores")
+@RequiredArgsConstructor
+public class AutorController implements GenericController {
+    private final AutorService service;
+    private final AutorMapper mapper;
+
+    @PostMapping
+    @PreAuthorize("hasRole('GERENTE')")
+    public ResponseEntity<Void> salvar(@RequestBody @Valid AutorDTO dto) {
+
+        Autor autor = mapper.toEntity(dto);
+        service.salvar(autor);
+        URI location = gerarHeaderLocation(autor.getId());
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("{id}")
+    @PreAuthorize("hasAnyRole('OPERADOR', 'GERENTE')")
+    public ResponseEntity<AutorDTO> obterDetalhes(@PathVariable("id") String id) {
+        var idAutor = UUID.fromString(id);
+
+        return service
+                .obterPorId(idAutor)
+                .map(autor -> {
+                    AutorDTO dto = mapper.toDTO(autor);
+                    return ResponseEntity.ok(dto);
+                }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    // indempontente
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('GERENTE')")
+    public ResponseEntity<Void> deletar(@PathVariable("id") String id) {
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
+
+        if (autorOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        service.deletar(autorOptional.get());
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('OPERADOR', 'GERENTE')")
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "nacionalidade", required = false) String nacionalidade) {
+        List<Autor> resultado = service.pesquisaByExample(nome, nacionalidade);
+        List<AutorDTO> lista = resultado
+                .stream()
+                .map(mapper::toDTO)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(lista);
+    }
+
+    @PutMapping("{id}")
+    @PreAuthorize("hasRole('GERENTE')")
+    public ResponseEntity<Void> atualizar(
+            @PathVariable("id") String id, @RequestBody @Valid AutorDTO dto) {
+
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
+
+        if (autorOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var autor = autorOptional.get();
+        autor.setNome(dto.nome());
+        autor.setNacionalidade(dto.nacionalidade());
+        autor.setDataNascimento(dto.dataNascimento());
+
+        service.atualizar(autor);
+
+        return ResponseEntity.noContent().build();
+
+    }
+}
