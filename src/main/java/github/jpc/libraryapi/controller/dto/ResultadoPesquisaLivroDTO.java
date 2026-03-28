d8c318ebc421cad290173088f2b3de3ab73e3060
joaopc <1232324@isep.ipp.pt>
2026-03-28 15:17:49 +0000

    add ResultadoPesquisaLivroDTO class for representing book search results

---
+++ src/main/java/github/jpc/libraryapi/controller/dto/ResultadoPesquisaLivroDTO.java
@@ -0,0 +1,18 @@
+package github.jpc.libraryapi.controller.dto;
+
+import github.jpc.libraryapi.model.GeneroLivro;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record ResultadoPesquisaLivroDTO(
+        UUID id,
+        String isbn,
+        String titulo,
+        LocalDate dataPublicacao,
+        GeneroLivro genero,
+        BigDecimal preco,
+        AutorDTO autor
+) {
+}
