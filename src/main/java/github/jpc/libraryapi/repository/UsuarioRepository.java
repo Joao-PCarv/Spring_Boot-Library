bae05a4c8b1d289ac558a935602ad090b732d97b
joaopc <1232324@isep.ipp.pt>
2026-03-28 15:17:18 +0000

    add UsuarioRepository interface for user data access

---
+++ src/main/java/github/jpc/libraryapi/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package github.jpc.libraryapi.repository;
+
+import github.jpc.libraryapi.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    Usuario findByLogin(String login);
+
+    Usuario findByEmail(String email);
+}
