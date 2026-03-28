c10c5cf1f2441968c4d1ae344448ac13a5f9c283
joaopc <1232324@isep.ipp.pt>
2026-03-28 15:19:55 +0000

    add auditing fields to Autor entity for tracking creation and modification dates

--- src/main/java/github/jpc/libraryapi/model/Autor.java
+++ src/main/java/github/jpc/libraryapi/model/Autor.java
@@ -4,8 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,27 +17,44 @@ import java.util.UUID;
 @Table(name="autor", schema = "public") // Especifica o nome da tabela no banco de dados
 @Getter // Gera os métodos getters para os atributos da classe
 @Setter
-@ToString
+@ToString(exclude = {"livros"})
+@EntityListeners(AuditingEntityListener.class)
 public class Autor {
 
     //Mapeamento dos atributos da classe para as colunas da tabela no banco de dados do autor
     // se a anotação @Table, todos os atributos da classe serão mapeados para as colunas da tabela
 
     @Id
-    @Column(name="id")
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name="nome", length = 100, nullable = false)
+    @Column(name = "nome", length = 100, nullable = false)
     private String nome;
 
     @Column(name = "data_nascimento", nullable = false)
-    private LocalDate dataNascimanto;
+    private LocalDate dataNascimento;
 
     @Column(name = "nacionalidade", length = 50, nullable = false)
     private String nacionalidade;
 
-    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL) // Especifica o relacionamento um-para-muitos entre Autor e Livro, onde "autor" é o nome do atributo na classe Livro que referencia o Autor
+    @OneToMany(mappedBy = "autor", fetch = FetchType.LAZY
+//            , cascade = CascadeType.ALL
+    )
     private List<Livro> livros;
 
+    @CreatedDate
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
 }
+
+
